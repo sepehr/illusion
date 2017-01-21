fc500e746747b0cf31a0aa048f81fb5a578cf3da
Sepehr Lajevardi <lajevardi@gmail.com>
2017-01-21 18:09:47 +0330

    Extract the bootstrap logic into multiple files.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,1 +1,30 @@
 <?php
+
+require_once 'autoload.php';
+
+use Illuminate\Config\Repository;
+use Illuminate\Container\Container;
+use Symfony\Component\Console\Application;
+
+define('BASE_PATH', realpath(__DIR__ . '/..'));
+define('LOGS_PATH', BASE_PATH . '/logs');
+define('CONFIG_PATH', BASE_PATH . '/config');
+
+$config = new Repository(require CONFIG_PATH . '/app.php');
+
+$container = new Container;
+$container->instance('config', $config);
+
+$app = new Application($config->get('name'), $config->get('version'));
+
+// Register container-aware commands
+$app->addCommands(array_map(function ($command) use ($container) {
+    return new $command($container);
+}, $config->get('commands.container-aware')));
+
+// Register non-container-aware commands
+$app->addCommands(array_map(function ($command) {
+    return new $command;
+}, $config->get('commands.non-container-aware')));
+
+return $app;

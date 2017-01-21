d7d432d6e7dc249ce86424ac15f7d3ccd92b2ae8
Sepehr Lajevardi <lajevardi@gmail.com>
2017-01-21 23:48:25 +0330

    Formatting.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,14 +7,21 @@ use Symfony\Component\Console\Application;
 $container   = require_once 'container.php';
 $application = new Application($container->config->get('name'), $container->config->get('version'));
 
-$application->addCommands(array_map(function ($command) use ($container) {
-    return new $command($container);
-}, $container->config->get('commands.container-aware')));
+$application->addCommands(array_map(
+    function ($command) use ($container) {
+        return new $command($container);
+    },
+    $container->config->get('commands.container-aware')
+));
 
-$application->addCommands(array_map(function ($command) {
-    return new $command;
-}, $container->config->get('commands.non-container-aware')));
+$application->addCommands(array_map(
+    function ($command) {
+        return new $command;
+    },
+    $container->config->get('commands.non-container-aware')
+));
 
+set_time_limit(0);
 date_default_timezone_set($container->config->get('timezone'));
 
 return $application;

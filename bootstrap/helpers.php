d7982dbe538a1b0ea5b0c499bc9b67b382819ac8
Sepehr Lajevardi <lajevardi@gmail.com>
2017-01-21 23:14:17 +0330

    Implement path helpers.

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -14,3 +14,45 @@ if (! function_exists('env')) {
         return getenv($key) ?: $default;
     }
 }
+
+if (! function_exists('base_path')) {
+    /**
+     * Returns the full absolute path to the base directory.
+     *
+     * @param  string $path
+     *
+     * @return string
+     */
+    function base_path($path = '')
+    {
+        return realpath(__DIR__ . '/..') . '/' . $path;
+    }
+}
+
+if (! function_exists('storage_path')) {
+    /**
+     * Returns the full absolute path to storage/ directory.
+     *
+     * @param  string $path
+     *
+     * @return string
+     */
+    function storage_path($path = '')
+    {
+        return base_path("storage/$path");
+    }
+}
+
+if (! function_exists('config_path')) {
+    /**
+     * Returns the full absolute path to config/ directory.
+     *
+     * @param  string $path
+     *
+     * @return string
+     */
+    function config_path($path = '')
+    {
+        return base_path("config/$path");
+    }
+}

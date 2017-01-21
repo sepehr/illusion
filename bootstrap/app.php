26fffc50ed23a9b607fbb43ca9f7e90c4b66f41d
Sepehr Lajevardi <lajevardi@gmail.com>
2017-01-21 23:13:37 +0330

    Container bindings for log, events, files and database components.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,25 +6,51 @@ use Illuminate\Config\Repository;
 use Illuminate\Container\Container;
 use Symfony\Component\Console\Application;
 
-define('BASE_PATH', realpath(__DIR__ . '/..'));
-define('LOGS_PATH', BASE_PATH . '/logs');
-define('CONFIG_PATH', BASE_PATH . '/config');
-
-$config = new Repository(require CONFIG_PATH . '/app.php');
-
 $container = new Container;
-$container->instance('config', $config);
+
+$container->instance('config', $config = new Repository(require config_path('app.php')));
+
+$container->singleton('files', function () {
+    return new Illuminate\Filesystem\Filesystem;
+});
+
+$container->singleton('events', function ($container) {
+    return new Illuminate\Events\Dispatcher($container);
+});
+
+$container->singleton('log', function () use ($config) {
+    $logger = new Illuminate\Log\Writer(
+        new Monolog\Logger($config->get('log.channel'))
+    );
+
+    $logger->useFiles($config->get('log.path'));
+
+    return $logger;
+});
+
+$container->singleton('db', function ($container) use ($config) {
+    $capsule = new Illuminate\Database\Capsule\Manager($container);
+
+    $capsule->addConnection($config->get('database'));
+    $capsule->setEventDispatcher($container->make('events'));
+
+    $capsule->setAsGlobal();
+    $capsule->bootEloquent();
+
+    return $capsule;
+});
 
 $app = new Application($config->get('name'), $config->get('version'));
 
-// Register container-aware commands
 $app->addCommands(array_map(function ($command) use ($container) {
     return new $command($container);
 }, $config->get('commands.container-aware')));
 
-// Register non-container-aware commands
 $app->addCommands(array_map(function ($command) {
     return new $command;
 }, $config->get('commands.non-container-aware')));
+
+// Misc. setup
+date_default_timezone_set($config->get('timezone'));
 
 return $app;

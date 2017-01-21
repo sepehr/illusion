47888a3db3d84b093968d24c42bad2a7c0775f72
Sepehr Lajevardi <lajevardi@gmail.com>
2017-01-21 15:25:27 +0330

    Add example command.

---
+++ src/Example/WelcomeCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Example;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class WelcomeCommand extends Command
+{
+    /**
+     * Command configurer.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('example:welcome')
+            ->setDescription('Greets you with a welcome message!')
+            ->setHelp('An example welcome command; feel free to remove this from bin/app.')
+            ->addArgument('name', InputArgument::OPTIONAL, 'Name to greet to.');
+    }
+
+    /**
+     * Command executor.
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     *
+     * @return void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $greet = ($greet = $input->getArgument('name'))
+            ? "Welcome to the machine, $greet!"
+            : 'Welcome to the machine!';
+
+        $output->writeln($greet);
+    }
+}
